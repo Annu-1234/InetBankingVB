c116af0f689a97af02c6894646b3363039591da5
Annu-1234 <77038224+Annu-1234@users.noreply.github.com>
2024-05-01 14:42:36 +0530

    Update newFile.java

--- Selenium_Intro/src/test/java/newFile.java
+++ Selenium_Intro/src/test/java/newFile.java
@@ -1,1 +1,12 @@
-//This is my new file created in github itself
+public class newFile {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://demoqa.com");
+		Thread.sleep(6000);
+
+  }
+}
